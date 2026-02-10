b8e94ec89a1fb479432d5f221e9e91fad5b50265
cconner57 <github.gothic155@passmail.net>
2026-02-10 09:51:18 -0800

    updated package

---
+++ src/components/common/PageLoader.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { useUIStore } from '../../stores/ui'
+
+const uiStore = useUIStore()
+const { isLoading } = storeToRefs(uiStore)
+</script>
+
+<template>
+  <div v-if="isLoading" class="page-loader">
+    <div class="bar"></div>
+  </div>
+</template>
+
+<style scoped>
+.page-loader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 3px;
+  background: transparent;
+  z-index: 9999;
+  pointer-events: none;
+}
+
+.bar {
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-primary);
+  transform-origin: left;
+  animation: loading 2s infinite;
+}
+
+@keyframes loading {
+  0% {
+    transform: scaleX(0);
+  }
+
+  50% {
+    transform: scaleX(0.5);
+  }
+
+  100% {
+    transform: scaleX(1);
+    opacity: 0;
+  }
+}
+</style>
